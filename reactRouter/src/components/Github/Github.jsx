162e3b8bf3c995b9b9624c22e4aca68355496ae1
sumanmajee <78966433+sumanmajee@users.noreply.github.com>
2024-04-04 16:39:40 +0530

    react router

---
+++ reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+
+    // const [data, setData] = useState([])
+
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         setData(data)
+    //     })
+    // }, [])
+
+    const data = useLoaderData()
+
+  return (
+    <div className=' text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+      Github Followers : {data.followers}
+      <img src={data.avatar_url} alt='git picture' width={300}></img>
+    </div>
+
+  )
+}
+
+export default Github
+
+
+export const githubInfoLoader = async() => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Route from './Route.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Route/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "about",
+        element: <About/>
+      },
+      {
+        path: "contact",
+        element: <Contact/>
+      },
+      {
+        path: "user/:id",
+        element: <User/>
+      },
+      {
+        loader: githubInfoLoader,
+        path: "github",
+        element: <Github/>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
